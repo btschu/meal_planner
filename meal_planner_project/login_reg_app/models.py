bda5459aa0e19d76cbe6230d823df49b9c9d8109
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-04-25 23:50:51 -0600

    Added validations to login and registration

--- meal_planner_project/login_reg_app/models.py
+++ meal_planner_project/login_reg_app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+import re
 
-# Create your models here.
+class UserManager(models.Manager):
+    def registration_validator(self, postData):
+        errors = {}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if len(postData['first_name']) < 1:
+            errors["first_name"] = "Please enter a FIRST NAME"
+        if len(postData['last_name']) < 1:
+            errors["last_name"] = "Please enter a LAST NAME"
+        if len(postData['email']) >= 1:
+            errors["email"] = "That EMAIL ADDRESS is already registered"
+        if not EMAIL_REGEX.match(postData['email']):
+            errors["email"] = "Please enter a valid EMAIL ADDRESS"
+        if len(postData['password']) < 8:
+            errors["password"] = "Your PASSWORD must be at least 8 characters long"
+        if not any(char.isdigit() for char in postData['password']):
+            errors["password"] = "Your PASSWORD must contain at least one number"
+        if not any(char.isupper() for char in postData['password']):
+            errors["password"] = "Your PASSWORD must contain at least one uppercase letter"
+        if postData['password'] != postData['confirm']:
+            errors["password"] = "Your PASSWORDS do not match"
+        return errors
+
+    def login_validator(self, postData):
+        errors = {}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = "Invalid email address or password!"
+        if (len(postData['password']) < 8):
+            errors['password'] = "Invalid email address or password!"
+        return errors
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
--- meal_planner_project/login_reg_app/views.py
+++ meal_planner_project/login_reg_app/views.py
@@ -1,7 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import User
+import bcrypt
 
-def login(request):
+def index(request):
     return render(request, 'login_reg_app/login.html')
 
+def login(request):
+    if request.method == 'GET':
+        return redirect('/')
+    if request.method == 'POST':
+        user = User.objects.filter(email=request.POST['email'])
+        errors = User.objects.login_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+            return redirect("/")
+        if user:
+            logged_user = user[0]
+            if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
+                request.session['userid'] = logged_user.id
+                return redirect('/dashboard')
+        return redirect("/")
+
 def registration(request):
-    return render(request, 'login_reg_app/registration.html')
+    if request.method == 'GET':
+        return render(request, 'login_reg_app/registration.html')
+    if request.method == 'POST':
+        password = request.POST['password']
+        pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        # print(pw_hash)
+        errors = User.objects.registration_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+            return redirect("/registration")
+        else:
+            User.objects.create(
+                first_name=request.POST['first_name'],
+                last_name=request.POST['last_name'],
+                email=request.POST['email'],
+                password=pw_hash
+            )
+            return redirect("/dashboard")
+
+def dashboard(request):
+    return render(request, 'login_reg_app/dashboard.html')
